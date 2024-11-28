a83761cb36bab18829fb7aac6d205cb33f39c0ea
Yang Feng <fengyang0317@gmail.com>
2024-11-28 15:36:56 +0800

    add rocm plugin

---
+++ plugins/rocm/setup.py
@@ -0,0 +1,17 @@
+import datetime
+import os
+import sys
+
+# add `build_util` to import path
+sys.path.append(os.path.join(os.path.dirname(__file__), '..', '..'))
+
+import build_util
+import setuptools
+
+build_util.bazel_build('@xla//xla/pjrt/c:pjrt_c_api_gpu_plugin.so',
+                       'torch_xla_rocm_plugin/lib', ['--config=rocm'])
+
+setuptools.setup(
+    # TODO: Use a common version file
+    version=os.getenv('TORCH_XLA_VERSION',
+                      f'2.5.0.dev{datetime.date.today().strftime("%Y%m%d")}'))
